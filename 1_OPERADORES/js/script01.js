7b50039016a8633fa1aa0e4657c9221eecfd610c
Crislei Aparecida da Silva <crisleisilva1905@gmail.com>
2024-06-22 15:44:17 -0300

    testes

---
+++ 1_OPERADORES/js/script01.js
@@ -0,0 +1,25 @@
+//1- Number
+console.log(typeof 2);
+console.log(typeof 8.20);
+console.log(typeof -132);
+// 2 - Ops. aritméticas
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+
+// 3 - Special Numbers
+console.log(typeof Infinity);
+
+console.log(12 * "asd");
+
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Um texto");
+console.log(`Mais um texto`);
+
+console.log(typeof "Opa");
+
+
